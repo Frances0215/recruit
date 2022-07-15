ba493950210e7e29127a23e0ad366242e6755e41
17893 <1789319416@qq.com>
2022-07-15 15:33:00 +0800

    6

--- src/components/home/StuActivityList.vue
+++ src/components/home/StuActivityList.vue
@@ -5,6 +5,7 @@
         <h3 class="medium">{{ item }}</h3>
       </el-carousel-item>
     </el-carousel>
+    <h1 ref="test" id="test">dd</h1>
     <el-table
       ref="multipleTable"
       :data="tableData"
@@ -12,37 +13,52 @@
       style="width: 100%"
       @selection-change="handleSelectionChange">
       <el-table-column
+        prop="id"
+        label="活动ID"
+        width="120">
+      </el-table-column>
+      <el-table-column
         prop="name"
         label="活动名称"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="date"
-        label="活动时间"
+        prop="star_time"
+        label="开始时间"
         width="120">
       </el-table-column>
       <el-table-column
-        prop="time"
+        prop="end_time"
+        label="结束时间"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="enroll_time"
         label="报名时间"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
-        prop="type"
+        prop="tag2"
+        label="进行方式"
+        show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+        prop="tag1"
         label="活动类型"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
-        prop="address"
+        prop="place"
         label="活动地区"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
-        prop="partner"
+        prop="limit_"
         label="参与对象"
         show-overflow-tooltip>
       </el-table-column>
       <el-table-column
-        prop="num"
+        prop="join_num"
         label="参与数"
         show-overflow-tooltip>
       </el-table-column>
@@ -51,52 +67,105 @@
         label="操作"
         width="100">
         <template slot-scope="scope">
-          <el-button @click="handleClick(scope.row)" type="text" size="small">查看详情</el-button>
+          <el-button @click="handleClick(scope.row),dialogTableVisible = true" type="text" size="small">查看详情</el-button>
         </template>
       </el-table-column>
     </el-table>
     <div style="margin-top: 20px">
-      <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>
-      <el-button @click="toggleSelection()">取消选择</el-button>
+<!--      <el-button @click="toggleSelection([tableData[1], tableData[2]])">切换第二、第三行的选中状态</el-button>-->
+<!--      <el-button @click="toggleSelection()">取消选择</el-button>-->
     </div>
-    <el-descriptions title="用户信息">
-      <el-descriptions-item label="用户名" ref="ename">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="手机号" ref="edate">18100000000</el-descriptions-item>
-      <el-descriptions-item label="居住地">苏州市</el-descriptions-item>
-      <el-descriptions-item label="备注">
-        <el-tag size="small">学校</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item label="联系地址">江苏省苏州市吴中区吴中大道 1188 号</el-descriptions-item>
-    </el-descriptions>
+    <el-dialog title="活动信息" :visible.sync="dialogTableVisible">
+      <el-descriptions class="margin-top"  :column="3" :size="size" border>
+        <template slot="extra">
+          <el-button type="primary" size="small">操作</el-button>
+        </template>
+        <el-descriptions-item label="活动ID">{{this.aid}}</el-descriptions-item>
+        <el-descriptions-item label="活动名">{{this.aname}}</el-descriptions-item>
+        <el-descriptions-item label="活动时间">{{ this.astart_time+'-'+this.aend_time }}</el-descriptions-item>
+        <el-descriptions-item label="报名时间">{{ this.aenroll_time }}</el-descriptions-item>
+        <el-descriptions-item label="活动图片">{{ this.afile }}</el-descriptions-item>
+        <el-descriptions-item label="活动描述">{{ this.atext }}</el-descriptions-item>
+        <el-descriptions-item label="活动附件">{{ this.afile }}</el-descriptions-item>
+      </el-descriptions>
+    </el-dialog>
   </div>
 
 </template>
 
 <script>
+var responses
+
 /* eslint-disable */
 export default {
+  detail:{name:'d'},
   data () {
-    var tabledata = []
     this.$axios.get('/auth/can_join/page=1').then(successResponse => {
       if (successResponse.data.code === 200) {
-        successResponse.data.result.content.forEach(function (item) {
-          var act = {name: item.name, date: item.creat_time}
-          tabledata.push(act)
-        })
-        console.log(tabledata)
+
+        this.tableData=successResponse.data.result.content
+        console.log(this.tableData)
       }
     })
       .catch(failResponse => {
       })
     return {
-      tableData: tabledata, multipleSelection: []
+      tableData: [],
+      multipleSelection: [],
+      dialogTableVisible: false,
+      aid:null,
+      astart_time:null,
+      aend_time:null,
+      atag1:null,
+      atag2:null,
+      atext:null,
+      atheme:null,
+      aname:null,
+      ajoin_num:null,
+      auid:null,
+      acreat_time:null,
+      areviewer:null,
+      alimit:null,
+      aenroll_time:null,
+      aplace:null,
+      afile:null
+
     }
   },
 
   methods: {
     handleClick(row) {
-      console.log();
-
+      this.aid=row.id
+      this.astart_time=row.star_time
+      this.aend_time=row.end_time
+      this.aname=row.name
+      this.aenroll_time=row.enroll_time
+      this.afile=row.files
+      this.atext=row.text
+      let param=new FormData
+      param.append("fid",17)
+      this.$axios.get('/file',param).then(successResponse => {
+        console.log(successResponse)
+      }.
+      // this.$refs.edate.innerText="a"
+      // this.$axios
+      //   .post('/no-authc/publisher/page=1', {
+      //     username: this.loginForm.username,
+      //     password: this.loginForm.password
+      //   })
+      //   .then(successResponse => {
+      //     console.log(successResponse)
+      //     if (successResponse.data.code === 200) {
+      //       // var data = this.loginForm
+      //       console.log('success')
+      //       // _this.$store.commit('login', _this.loginForm)
+      //       var path = this.$route.query.redirect
+      //       // this.$router.replace({path: path === '/' || path === undefined ? '/index' : path})
+      //       // this.$router.replace({path: '/index'})
+      //     }
+      //   })
+      //   .catch(failResponse => {
+      //   })
     },
     toggleSelection (rows) {
       if (rows) {
